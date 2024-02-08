2f4f45cad8e1ce63ff09c3dd0f0ec009d83ce6c2
Brandon Fenske <bafenske@asu.edu>
2024-02-08 14:42:37 -0700

    highlight buttons

--- HCUI.py
+++ HCUI.py
@@ -4,7 +4,56 @@ from kivy.core.window import Window
 from kivy.utils import get_color_from_hex
 from kivy.uix.popup import Popup 
 from kivy.uix.label import Label
-#imports here
+from kivy.clock import Clock
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.widget import Widget
+from kivy.graphics import Color, Line, RoundedRectangle
+
+class RoundedButton(ButtonBehavior, Label):
+    background_normal_color = get_color_from_hex('#1F448C')
+    background_down_color = get_color_from_hex('#3F668C')  # A slightly different color for the pressed state
+    border_color = get_color_from_hex('#FFFFFF')  # Set the border color here
+
+    def __init__(self, **kwargs):
+        super(RoundedButton, self).__init__(**kwargs)
+        self._trigger = None
+        with self.canvas.before:
+            Color(rgba=self.background_normal_color)
+            self.bg_rect = RoundedRectangle(size=self.size, pos=self.pos, radius=[15])
+        with self.canvas.after:
+            Color(rgba=self.border_color)
+            self.border = Line(rounded_rectangle=(self.x, self.y, self.width, self.height, 15), width=1.5)
+
+    def on_size(self, *args):
+        self.bg_rect.size = self.size
+        self.border.rounded_rectangle = (self.x, self.y, self.width, self.height, 15)
+
+    def on_pos(self, *args):
+        self.bg_rect.pos = self.pos
+        self.border.rounded_rectangle = (self.x, self.y, self.width, self.height, 15)
+
+    def on_press(self):
+        self.canvas.before.clear()
+        with self.canvas.before:
+            Color(rgba=self.background_down_color)
+            RoundedRectangle(size=self.size, pos=self.pos, radius=[15])
+
+    def on_release(self):
+        self.canvas.before.clear()
+        with self.canvas.before:
+            Color(rgba=self.background_normal_color)
+            RoundedRectangle(size=self.size, pos=self.pos, radius=[15])
+        self.cancel_activation()
+
+    def activate_button(self, dt):
+        # Add the code here that should be executed after the button is held for 4 seconds
+        print("Button activated")  # Example action
+
+    def cancel_activation(self):
+        if self._trigger:
+            Clock.unschedule(self._trigger)
+            self._trigger = None
+
 
 class InfoPopup(Popup): 
     pass
