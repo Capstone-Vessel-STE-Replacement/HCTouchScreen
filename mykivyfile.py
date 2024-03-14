69f6c8b35f63c02f52e4966232d58a10f5172f32
Brandon Fenske <bafenske@asu.edu>
2024-03-14 12:38:31 -0700

    data log

--- mykivyfile.py
+++
@@ -1,9 +0,0 @@
-from kivymd.app import MDApp
-from kivymd.uix.button import MDFlatButton
-
-class TestMDApp(MDApp):
-    def build(self):
-        return MDFlatButton(text='Hello, KivyMD!', pos_hint={'center_x': 0.5, 'center_y': 0.5})
-
-if __name__ == '__main__':
-    TestMDApp().run()
